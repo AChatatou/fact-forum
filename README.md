0b33d50e88e58cc4c8a1d2cbdaa93814ad913888
A.CH. <adil.chtatou@gmail.com>
2025-04-27 21:42:12 +0200

    adding production build and website URL to Readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fact-forum
-A Website where you can share facts and react to them 
+
+A Website where you can share facts and react to them
+The Website is available on: https://factforum.netlify.app
---
+++ build/static/js/952.29363498.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfact_forum=self.webpackChunkfact_forum||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.29363498.chunk.js.map
